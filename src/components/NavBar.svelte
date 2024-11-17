e7ff7f3fe7a42cca066c92955f911f41df011656
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-11-17 03:18:23 +0200

    Update NavBar.svelte

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,40 +1,37 @@
 <script>
-	import { goto } from '$app/navigation';
-    import {page} from "$app/stores";
+  import {goto} from "$app/navigation";
+  import {page} from "$app/stores";
 </script>
 
-
-  
-    <div
-    class="w-full bg-opacity-50 bg-black z-50 items-center text-white flex px-5 justify-between pb-2 xl:px-10 pt-6 md:pt-4 xl:pt-2 mb-5 mt-1" 
+<div
+  class="w-full bg-opacity-50 bg-black z-50 items-center text-white flex px-5 justify-between pb-2 xl:px-10 pt-6 md:pt-4 xl:pt-2 mb-5 mt-1"
+>
+  <a
+    class="hover:text-red-300 transition duration-200 h3"
+    href="https://amr-elsherbiny.vercel.app/"><span>AmrElsherbiny</span></a
   >
-   
-    <a class="hover:text-red-300 transition duration-200 h3" href="https://amr-elsherbiny.vercel.app/"
-  
-      ><span>AmrElsherbiny</span></a
-    >
 
-
-    <div
-      class="text-center xl:gap-20 text-sm xl:text-2xl md:text-xl flex bg-black bg-opacity-5 justify-around gap-3"
-    >
-    
-      <a class="hover:text-red-300 transition duration-200" href="#"
-      onclick={()=>goto('/Calculator')}
+  <div
+    class="text-center xl:gap-20 text-sm xl:text-2xl md:text-xl flex bg-black bg-opacity-5 justify-around gap-3"
+  >
+    <a
+      class="hover:text-red-300 transition duration-200"
+      href="#"
+      onclick={() => goto("/Calculator")}
       class:active={$page.url.pathname === "/Calculator"}
-        ><span>Calculator</span></a
-      >
-      <a class="hover:text-red-300 transition duration-200" href="#"
-      onclick={()=>goto('/History')}
-      class:active={$page.url.pathname === "/History"}
-        ><span>History</span></a
-      >
-
-    </div>
+      ><span>Calculator</span></a
+    >
+    <a
+      class="hover:text-red-300 transition duration-200"
+      href="#"
+      onclick={() => goto("/History")}
+      class:active={$page.url.pathname === "/History"}><span>History</span></a
+    >
   </div>
+</div>
 
 <style>
-     .active {
+  .active {
     @apply variant-filled-primary px-3 rounded-md;
   }
 </style>
